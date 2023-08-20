e12cc5adbe0673e842abd466d0c149d5a4c17364
m-e-st <michael@stumpp.name>
2023-08-20 18:22:58 +0200

    Easter Egg #1

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "DB S&S Tools",
 	description: "TIM Tool für DB Station&service AG",
 	caption: "TIM",
-	version : "0.6.91",
+	version : "0.6.93",
 	mail: "SuS.Gebaeudeautomation@deutschebahn.com",
 
 	url : {
